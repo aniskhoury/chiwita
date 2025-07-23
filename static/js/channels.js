115ac9e50f61f1e1f16e93fdebaad580ca3e9cf8
Anïs Khoury <akhouryribas@gmail.com>
2025-07-23 19:29:12 +0200

    Add name and counter users online on showchannels

--- static/js/channels.js
+++ static/js/channels.js
@@ -4,7 +4,7 @@ function showListChannels(){
 
 
   $.ajax({
-        url: 'http://localhost:8080/listChannels',
+        url: '/listChannels',
     dataType: 'application/json',
     complete: function(data){
         console.log(data.responseText)
@@ -13,6 +13,11 @@ function showListChannels(){
         console.log(result);
         var resultJSON = JSON.parse(result);
         console.log(resultJSON);
+        var content = document.getElementById("showChannels") ;
+
+        for (var i =0;i<resultJSON.length;i++){
+            content.innerHTML =content.innerHTML+ resultJSON[i]["channel"]+" users online: "+resultJSON[i]["counterUsers"]+"<br>";
+        }
     },
     success: function(data){
         console.log(data)
